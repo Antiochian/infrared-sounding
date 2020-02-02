554b5362c983f009c6b8356721ffc8aa15c048eb
Antioch <53316182+Antiochian@users.noreply.github.com>
2020-02-02 10:31:19 +0000

    Update get_T.m

--- get_T.m
+++ get_T.m
@@ -1,4 +1,6 @@
 function [ output ] = get_T (B,nu)
+%Crude algorithm to find T, given B (inverse Planck function)
+
 allowed_error = 0.0001;
 converge_speed = 2;
 step_size = 5;
@@ -14,14 +16,14 @@ prevflag = sign(eval);
 
 bool = 0;
 while bool == 0
-    if eval == 0 %this will never happen
+    if eval == 0 %this will never happen, but just in case
         bool = 1;
         output = Tguess;
     elseif sign(eval) ~= prevflag
         %sign change!
         if abs(step_size) < allowed_error
             bool = 1;
-            output = Tguess;
+            output = Tguess; %this will eventually be the result
         else
             step_size = -1*step_size/converge_speed;
             Tguess = Tguess + step_size;
@@ -32,7 +34,5 @@ while bool == 0
     prevflag = sign(eval);
     Bguess = get_B(Tguess,nu);
     eval = Bt-Bguess;   
-      
-%T loosely proprotinal to B
 
 end
